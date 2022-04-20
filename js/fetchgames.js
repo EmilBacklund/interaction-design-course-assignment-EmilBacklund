3996f60e920a3972cbafed6d55d795ff9050538f
Emil Backlund <Emil.backlund@stud.noroff.no>
2022-04-20 11:50:09 +0200

    added loading indicator
    going to start with detail page next

--- js/fetchgames.js
+++ js/fetchgames.js
@@ -19,14 +19,18 @@ async function fetchGames() {
       if (!gameData[i].on_sale) {
         gameContainer.innerHTML += `
         <div>
+        <a href="">
         <img class="game-images" src="${gamePicture}" alt="${altText}">
+        </a>
         <div class="game-card">
         <div>
         <p>${gameData[i].name}</p>
         <p>${gamePrice} ${currency}</p>
         </div>
         <div>
-        <button class="cta-button">Buy Digital</button>
+        <a href="">
+            <div class="buy-button">Buy Digital</div>
+          </a>
         </div>
         </div>
         </div>
@@ -34,7 +38,9 @@ async function fetchGames() {
       } else if (gameData[i].on_sale) {
         marketContainer.innerHTML += `
           <div class="market__box">
+          <a href="">
           <img class="market-images" src="${gamePicture}" alt="${altText}">
+          </a>
           <div class="market__game-details">
           <div>
           <p style="font-weight: 600">${gameData[i].name}</p>
@@ -52,7 +58,9 @@ async function fetchGames() {
           <p class="exo-smaller">DrBrave</p>
           </div>
           <div>
-          <button class="cta-button">Buy Digital</button>
+          <a href="">
+            <div class="buy-button">Buy Digital</div>
+          </a>
           </div>
           </div>
           </div>
