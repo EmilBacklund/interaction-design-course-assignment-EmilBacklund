0c86a8b1b8ebd3cde135bdf9226b2f51d80af719
EmilBacklund <Emil.backlund@stud.noroff.no>
2022-04-24 12:20:49 +0200

    small fixes

--- js/game-details.js
+++ js/game-details.js
@@ -1,14 +1,15 @@
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 
-const id = params.get("id");
+const id = params.get('id');
 
 const url =
-  "https://emilbacklund.flywheelsites.com/wp-json/wc/store/products/" + id;
+  'https://emilbacklund.flywheelsites.com/wp-json/wc/store/products/' + id;
 
-const breadcrumbGameName = document.querySelector(".breadcrumb-game-name");
-const gameHeading = document.querySelector(".game-detail-heading");
-const gameDetailBox = document.querySelector(".detail-youtube-box");
+const breadcrumbGameName = document.querySelector('.breadcrumb-game-name');
+const gameHeading = document.querySelector('.game-detail-heading');
+const gameDetailBox = document.querySelector('.detail-youtube-box');
+const description = document.querySelector('.description');
 
 async function fetchDetails() {
   try {
@@ -17,8 +18,12 @@ async function fetchDetails() {
 
     console.log(detailData);
     console.log(detailData.images[0].src);
+    console.log(detailData.name);
 
     let detailPicture = detailData.images[1].src;
+    let gamePrice = detailData.prices.price;
+    let currency = detailData.prices.currency_code;
+    let detailDescription = detailData.description;
 
     breadcrumbGameName.innerHTML = `<a href="javascript:window.location.reload(true)">${detailData.name}</a>`;
     gameHeading.innerHTML = `${detailData.name}`;
@@ -41,12 +46,17 @@ async function fetchDetails() {
       <p class="wish-list-text">Add to wish list</p>
     </div>
     <div>
-      <p class="game-cost-text">500 kr</p>
+      <p class="game-cost-text">${gamePrice} ${currency}</p>
       <a href="/payment-page.html"
         ><button class="cta-button">Buy Digital</button></a
       >
     </div>
   </div>`;
+
+    description.innerHTML = `
+  <h2>Description:</h2>
+            ${detailDescription}
+  `;
   } catch (error) {}
 }
 
